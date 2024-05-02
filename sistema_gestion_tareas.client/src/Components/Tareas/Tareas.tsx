c37505525c8b4ecfbb1c6e384588cfa4141d92e0
Johann Casallas <casallasbecerrajohann@gmail.com>
2024-05-02 10:08:30 -0500

    creacion del componente de Navegacion y Sepracion de logica y vista en componenete Tareas

--- sistema_gestion_tareas.client/src/Components/Tareas/Tareas.tsx
+++ sistema_gestion_tareas.client/src/Components/Tareas/Tareas.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import { IUsuario, IUsuarioInicial } from '../Interfaces/IUsuario';
-import { UsuarioService } from '../Servicios/usuarioService';
-import { Autocomplete, Backdrop, Button, CircularProgress, Grid, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField } from '@mui/material';
-import SearchIcon from '@mui/icons-material/Search';
-import { TareaService } from '../Servicios/tareaService';
-import { ITarea } from '../Interfaces/ITarea';
-import DeleteIcon from '@mui/icons-material/Delete';
-import EditIcon from '@mui/icons-material/Edit';
+import { IUsuario, IUsuarioInicial } from '../../Interfaces/IUsuario';
+import { ITarea } from '../../Interfaces/ITarea';
+import { UsuarioService } from '../../Servicios/usuarioService';
+import { TareaService } from '../../Servicios/tareaService';
+import TareasVista from './tareasVista';
 
 
 const Tareas = () => {
@@ -45,67 +42,13 @@ const Tareas = () => {
 
     return (
         <>
-            <Backdrop
-                sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-                open={isLoading}
-            >
-                <CircularProgress color="inherit" />
-            </Backdrop>
-        <Grid container spacing={2}>
-                <Grid item xs={10}>
-                    <Autocomplete
-                        disablePortal
-                        id="NombreUsuario"
-                        options={usuarios.map(usuario => usuario.nombreUsuario!)}
-                        renderInput={(params) => <TextField {...params} label="Nombre de Usuario" />}
-                        onChange={alCambiarValorAutocomplete}
-                    />
-                </Grid>
-                <Grid item xs={2}>
-                    <Button
-                        variant="outlined"
-                        startIcon={<SearchIcon />}
-                        onClick={obtenerTareasPorUsuario}
-                    >
-                        Buscar
-                    </Button>
-                </Grid>
-                {tareas.length > 0 && (
-                    <Grid item xs={12} >
-                        <TableContainer component={Paper}>
-                            <Table >
-                                <TableHead>
-                                    <TableRow>
-                                        <TableCell>Tarea</TableCell>
-                                        <TableCell>Descripcion</TableCell>
-                                        <TableCell>Nivel</TableCell>
-                                        <TableCell>Estado</TableCell>
-                                        <TableCell>Acciones</TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    {tareas.map((tarea) => (
-                                        <TableRow key={tarea.nombreTarea}>
-                                            <TableCell>{tarea.nombreTarea}</TableCell>
-                                            <TableCell>{tarea.descripcionTarea}</TableCell>
-                                            <TableCell>{tarea.nivelTarea}</TableCell>
-                                            <TableCell>{tarea.estadoTarea ? 'Finalizada' : 'Abierta'}</TableCell>
-                                            <TableCell>
-                                                <Button size="small" variant="contained" startIcon={<DeleteIcon />}>
-                                                    Eliminar
-                                                </Button>
-                                                <Button size="small" variant="contained" startIcon={<EditIcon />}>
-                                                    Editar
-                                                </Button>
-                                            </TableCell>
-                                        </TableRow>
-                                    ))}
-                                </TableBody>
-                            </Table>
-                        </TableContainer>
-                    </Grid>
-                )}
-            </Grid>
+            <TareasVista
+                alCambiarValorAutocomplete={alCambiarValorAutocomplete}
+                obtenerTareasPorUsuario={obtenerTareasPorUsuario}
+                isLoading={isLoading}
+                usuarios={usuarios}
+                tareas={tareas}
+            />
         </>
     );
 }
---
+++ sistema_gestion_tareas.client/src/Components/Tareas/tareasVista.tsx
@@ -0,0 +1,107 @@
+import React from 'react';
+
+import {
+    Autocomplete,
+    Backdrop, Button,
+    CircularProgress,
+    Grid,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    TextField
+} from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import { ITarea } from '../../Interfaces/ITarea';
+import { IUsuario } from '../../Interfaces/IUsuario';
+
+
+interface TareasVistaProps {
+    alCambiarValorAutocomplete: (_event: React.SyntheticEvent, newValue: string | null) => void;
+    obtenerTareasPorUsuario: () => Promise<void>;
+    isLoading: boolean;
+    usuarios: IUsuario[];
+    tareas: ITarea[];
+}
+
+const TareasVista: React.FC<TareasVistaProps> = ({ 
+    alCambiarValorAutocomplete,
+    obtenerTareasPorUsuario,
+    isLoading,
+    usuarios,
+    tareas
+}) => {
+    
+    return (
+        <>
+            <Backdrop
+                sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                open={isLoading}
+            >
+                <CircularProgress color="inherit" />
+            </Backdrop>
+            <Grid container spacing={2}>
+                <Grid item xs={10}>
+                    <Autocomplete
+                        disablePortal
+                        id="NombreUsuario"
+                        options={usuarios.map(usuario => usuario.nombreUsuario!)}
+                        renderInput={(params) => <TextField {...params} label="Nombre de Usuario" />}
+                        onChange={alCambiarValorAutocomplete}
+                    />
+                </Grid>
+                <Grid item xs={2}>
+                    <Button
+                        variant="outlined"
+                        startIcon={<SearchIcon />}
+                        onClick={obtenerTareasPorUsuario}
+                    >
+                        Buscar
+                    </Button>
+                </Grid>
+                {tareas.length > 0 && (
+                    <Grid item xs={12} >
+                        <TableContainer component={Paper}>
+                            <Table >
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>Tarea</TableCell>
+                                        <TableCell>Descripcion</TableCell>
+                                        <TableCell>Nivel</TableCell>
+                                        <TableCell>Estado</TableCell>
+                                        <TableCell>Acciones</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {tareas.map((tarea) => (
+                                        <TableRow key={tarea.nombreTarea}>
+                                            <TableCell>{tarea.nombreTarea}</TableCell>
+                                            <TableCell>{tarea.descripcionTarea}</TableCell>
+                                            <TableCell>{tarea.nivelTarea}</TableCell>
+                                            <TableCell>{tarea.estadoTarea ? 'Finalizada' : 'Abierta'}</TableCell>
+                                            <TableCell>
+                                                <Button size="small" variant="contained" startIcon={<DeleteIcon />}>
+                                                    Eliminar
+                                                </Button>
+                                                <Button size="small" variant="contained" startIcon={<EditIcon />}>
+                                                    Editar
+                                                </Button>
+                                            </TableCell>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </Grid>
+                )}
+            </Grid>
+        </>
+    );
+}
+
+export default TareasVista;
